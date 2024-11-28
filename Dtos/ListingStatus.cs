a960940afbc2401ddb0da6fd08b08659b4aadc0e
Jose E Rodriguez-Marrero <jose.rodriguez-marrero@proton.me>
2024-11-27 21:40:34 -0800

    Create status dto and status endpoint

---
+++ Dtos/ListingStatus.cs
@@ -0,0 +1,11 @@
+namespace AsyncApi.Dtos
+{
+    public class ListingStatus
+    {
+        public string? RequestStatus { get; set; }
+
+        public string? EstimatedCompletionTime { get; set; }
+
+        public string? ResourceUrl { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using AsyncApi.Data;
+using AsyncApi.Dtos;
 using AsyncApi.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,6 +24,30 @@ app.MapPost("/api/v1/products", async (AppDbContext context, ListingRequest list
     await context.SaveChangesAsync();
 
     return Results.Accepted($"api/v1/productstatus/{listingRequest.RequestId}", listingRequest);
+});
+
+// Status Endpoint
+app.MapGet("api/v1/productstatus/{requestId}", (AppDbContext context, string requestId) => {
+
+    var listingRequest = context.ListingRequests.FirstOrDefault(lr => lr.RequestId == requestId);
+
+    if (listingRequest is null) return Results.NotFound();
+
+    ListingStatus listingStatus = new ListingStatus
+    {
+        RequestStatus = listingRequest.RequestStatus,
+        ResourceUrl = string.Empty
+    };
+
+    if (listingRequest.RequestStatus!.ToUpper() == "COMPLETE")
+    {
+        listingStatus.ResourceUrl = $"api/v1/products/{Guid.NewGuid().ToString()}";
+        return Results.Ok(listingStatus);
+    }
+
+    listingStatus.EstimatedCompletionTime = "2024-11-26:14:11:11";
+
+    return Results.Ok(listingStatus);
 });
 
 app.Run();
